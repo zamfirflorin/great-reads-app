bd2a7c6d7c0901551034322bd362a699a985c4d1
FZAMFIR <zamfirflorinstelian@yahoo.com>
2022-05-02 13:30:44 +0300

    test

--- src/main/java/devmind/greatreadsapp/config/SecurityConfiguration.java
+++ src/main/java/devmind/greatreadsapp/config/SecurityConfiguration.java
@@ -1,4 +1,28 @@
 package devmind.greatreadsapp.config;
 
-public class SecurityConfiguration {
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.anyRequest().permitAll());
+        http.formLogin();
+        http.httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 }
